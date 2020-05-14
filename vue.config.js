8adee0de5dcb84a228766e58409b3e67eb1b6dca
piupuer <396679132@qq.com>
2020-05-14 10:15:10 +0800

    change name => gin-web-vue

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ const path = require('path')
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
 // For example, on Mac: sudo npm run / sudo yarn
-const devServerPort = 9527 // TODO: get this variable from setting.ts
-const mockServerPort = 9528 // TODO: get this variable from setting.ts
+const devServerPort = 8888 // TODO: get this variable from setting.ts
+const mockServerPort = 9128 // TODO: get this variable from setting.ts
 const name = 'Vue Typescript Admin' // TODO: get this variable from setting.ts
 
 module.exports = {
