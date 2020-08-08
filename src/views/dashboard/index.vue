d5d913ce1e6ec7418eddf822043f881a3ab8d5a7
piupuer <396679132@qq.com>
2020-08-08 11:56:57 +0800

    add index advertisement

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="app-container">
-    欢迎使用
+    <div class="tips">
+      <h5>以下是目前找到的性价比最高的服务器, 首次成功购买我能得到一点奖励, 非常感谢, 以后有更好的会推荐给大家</h5>
+      <p>UCloud国内服务器(领劵最低只要<span class="special">88元/年</span>):</p>
+      <p>
+        <el-link
+          href="https://www.ucloud.cn/site/active/1111.html?invitation_code=C1x8863BB6C7A1E"
+          target="_blank"
+          type="primary"
+        >
+          https://www.ucloud.cn/site/active/1111.html?invitation_code=C1x8863BB6C7A1E
+        </el-link>
+      </p>
+      <p>UCloud全球<span class="special">免备案</span>服务器(领劵最低只要<span class="special">150元/年</span>):</p>
+      <p>
+        <el-link
+          href="https://www.ucloud.cn/site/global.html?invitation_code=C1x8863BB6C7A1E"
+          target="_blank"
+          type="primary"
+        >
+          https://www.ucloud.cn/site/global.html?invitation_code=C1x8863BB6C7A1E
+        </el-link>
+      </p>
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+  .tips {
+    padding: 8px 16px;
+    background-color: #ecf8ff;
+    border-radius: 4px;
+    border-left: 5px solid #50bfff;
+    margin: 20px 0;
+    p {
+      font-size: 12px;
+    }
+    .special {
+      color: red;
+    }
+  }
+</style>
