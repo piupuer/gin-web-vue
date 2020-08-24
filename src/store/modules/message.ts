6dd02fa25ec0ed4006ad94aac67f6d0d49bcc8d9
piupuer <396679132@qq.com>
2020-08-24 20:56:37 +0800

    change message

--- src/store/modules/message.ts
+++ src/store/modules/message.ts
@@ -39,6 +39,7 @@ class Message extends VuexModule implements IMessageState {
     try {
       data = JSON.parse(data)
       switch (data.type) {
+        // 心跳
         case 0:
           if (data.detail && data.detail.code === 201) {
             this.Send({
@@ -47,8 +48,8 @@ class Message extends VuexModule implements IMessageState {
             })
           }
           break
+        // 新消息
         case 3:
-          console.log(data.detail)
           if (data.detail && data.detail.code === 201 && data.detail.data) {
             if (this.init && this.unReadCount < data.detail.data.unReadCount) {
               Notification({
@@ -62,13 +63,37 @@ class Message extends VuexModule implements IMessageState {
             }
             this.SetUnReadCount(data.detail.data.unReadCount)
             this.SET_INIT(true)
+            // 回复心跳
+            this.Send({
+              type: 0
+            })
           }
           break
-        default:
-          console.log('类型错误')
+        // 新用户上线
+        case 4:
+          if (data.detail && data.detail.code === 201 && data.detail.data) {
+            if (data.detail.data.user) {
+              const user = data.detail.data.user
+              Notification({
+                title: '上线提醒',
+                message: `用户${user.nickname}(${user.username})刚刚上线啦`,
+                type: 'success',
+                duration: 2000
+              })
+            }
+            // 回复心跳
+            this.Send({
+              type: 0
+            })
+          }
+          break
       }
     } catch (e) {
-      console.log('非json数据, 按字符串处理')
+      // 回复心跳
+      this.Send({
+        type: 0,
+        data
+      })
     }
   }
 
@@ -84,6 +109,11 @@ export const MessageModule = getModule(Message)
 // 连接建立之后执行send方法发送数据
 websocket.onopen = (e: any) => {
   console.log('已建立连接', e)
+}
+
+// 连接关闭后执行
+websocket.onclose = (e: any) => {
+  console.log('连接已断开', e)
 }
 
 // 监听消息接收
