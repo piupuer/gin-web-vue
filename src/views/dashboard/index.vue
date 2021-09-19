337842ac5f719a9237555cf42c7c0e64d7e03bb6
piupuer <396679132@qq.com>
2021-09-19 13:34:33 +0800

    bug fix

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -4,12 +4,13 @@
       <h1>欢迎使用Gin Web</h1>
       <h2>点击左侧菜单可试用系统各项功能</h2>
       <h2>有问题欢迎提Issue或QQ群交流</h2>
+      <h2>科学上网联系群主(每月<span class="special">50G免费流量</span>)</h2>
       <br>
       <br>
       <br>
       <br>
       <br>
-      <h5>广告: 便宜主机抢购</h5>
+      <h5>广告: </h5>
       <p>UCloud国内服务器(领劵最低只要<span class="special">59元/年</span>):</p>
       <p>
         <el-link
@@ -18,16 +19,6 @@
           type="primary"
         >
           https://www.ucloud.cn/site/active/kuaijie.html?invitation_code=C1x8863BB6C7A1E
-        </el-link>
-      </p>
-      <p>UCloud全球<span class="special">免备案</span>服务器(领劵最低只要<span class="special">150元/年</span>):</p>
-      <p>
-        <el-link
-          href="https://www.ucloud.cn/site/global.html?invitation_code=C1x8863BB6C7A1E"
-          target="_blank"
-          type="primary"
-        >
-          https://www.ucloud.cn/site/global.html?invitation_code=C1x8863BB6C7A1E
         </el-link>
       </p>
     </div>
